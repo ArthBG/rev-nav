8c6d0dd2b13bc9675db55fe786c8364f37181a39
Arthur Borges <arthurborges.senai564@gmail.com>
2024-03-11 16:33:03 -0300

    Add tab bar icons and customize tab bar style

--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -2,15 +2,46 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
 import Category from "../screens/Category";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 const TabRoutes = () => {
     return (
-        <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Profile" component={Profile} />
-        <Tab.Screen name="Category" component={Category} />
+        <Tab.Navigator
+        screenOptions={{
+            headerShown: false,
+            tabBarStyle: {
+                backgroundColor: '#EEF',
+            }
+        
+        }}
+        >
+        <Tab.Screen name="Home" component={Home} options={
+            {
+                tabBarIcon: ({focused}) => (
+                    <Feather name="home" size={24} color={focused ? "black" : "gray"} />
+                )
+            }
+        
+        } />
+        <Tab.Screen name="Profile" component={Profile} options={
+            {
+                tabBarIcon: ({focused}) => (
+                    <Feather name="user" size={24} color={focused ? "black" : "gray"} />
+                )
+            }
+        
+        
+        }/>
+        <Tab.Screen name="Category" component={Category} options={
+            {
+                tabBarIcon: ({focused}) => (
+                    <MaterialCommunityIcons name="format-list-bulleted" size={24} color={focused ? "black" : "gray"} />
+                )
+            }
+        }/>
         </Tab.Navigator>
     );
     }
