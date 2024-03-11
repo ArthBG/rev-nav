e6e773bd83d844bfa857960e53c0dc8655833b9c
Arthur Borges <arthurborges.senai564@gmail.com>
2024-03-11 14:46:20 -0300

    Add Home screen component and styles

---
+++ src/screens/Home/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import Title from '../../components/Title'
+import styles from './styles'
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Title title="Home" />
+    </View>
+  )
+}
+
+export default Home
